f2dd25f6a58d6a15f462e16aba9e4ccc3e266ea3
jaysonmk2 <76066308+jaysonmk2@users.noreply.github.com>
2021-08-06 12:19:04 -0700

    palindrom

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,1 +1,25 @@
-print("hello")
+from typing import List
+
+
+strings ="racecar"
+
+
+liststr = list()
+for letter in strings:
+    liststr += letter
+
+
+liststr.reverse()
+
+joined = "".join(liststr)
+
+
+
+if joined == strings:
+    print("is palindrome")
+else:
+    print("is not palindrome")
+
+
+
+
